2b74b59f8a440764aa7a319319c51420c6bd2c20
NMRayhan <nurmdrayhan97@gmail.com>
2020-01-27 19:04:04 +0600

    Variable Data Type check

--- 1stProgram.py
+++ 1stProgram.py
@@ -1,3 +1,14 @@
 number1 = 10
-number2 = 20
-print(number1+number2)
+number2 = "Hello"
+n = type(number2)
+print(n)
+n = type(number1)
+print(n)
+n = type(002.972)
+print(n)
+n = type(bool)
+print(n)
+a = 1
+b = 3.4
+print(a+b)
+#result = number1+number2
