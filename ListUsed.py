872989e041014c067d30e75faedb6e093c437c44
NMRayhan <nurmdrayhan97@gmail.com>
2020-01-28 19:17:08 +0600

    Math Function

--- ListUsed.py
+++ ListUsed.py
@@ -1,3 +1,4 @@
+import math
 Number = [1,2,3,4,5,6,7,8,9,10]
 print(Number)
 print(Number[0])
@@ -8,3 +9,8 @@ print("Total Number is This List ",len(Number))
 print(3 in Number)
 print(10 in Number)
 print(11 not in Number)
+a = Number[3]
+b = Number[8]
+print(math.floor(b/a))
+print(math.ceil(b/a))
+print(b//a)
