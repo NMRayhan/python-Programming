8c7da094d000cd7f979da4340e52986ab38f0d22
NMRayhan <nurmdrayhan97@gmail.com>
2020-01-28 23:03:24 +0600

    Type Casting Second Commit

--- TypeCasting.py
+++ TypeCasting.py
@@ -1,5 +1,5 @@
-num1 = input("Enter first Number : ")
-num2 = input("Enter Second Number : ")
+num1 = int(input("Enter first Number : "))
+num2 = int(input("Enter Second Number : "))
 
-sum = int(num1) + int(num2)
+sum = num1 + num2
 print("Sum of this two Number : ",sum)
