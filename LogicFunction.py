670c6c0681af029ba15db7d4c4c662cdf1a79190
NMRayhan <nurmdrayhan97@gmail.com>
2020-01-29 20:18:49 +0600

    Type Casting Third Commit

--- LogicFunction.py
+++ LogicFunction.py
@@ -3,3 +3,9 @@ print("Bang" != "BanG")
 Rslt = "Bagladesh" == "Bangladesh"
 number = bool(Rslt)
 print(number)
+Base = float(input("Enter Base : "))
+height = float(input("Enter Height : "))
+
+area = .5*Base*height
+a = int(area) #type kasting
+print(a)
