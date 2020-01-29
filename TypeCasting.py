7294ec34622ca6e80b13643064bf2906295ae334
NMRayhan <nurmdrayhan97@gmail.com>
2020-01-30 00:15:54 +0600

    Math Function

--- TypeCasting.py
+++ TypeCasting.py
@@ -3,3 +3,12 @@ num2 = int(input("Enter Second Number : "))
 
 sum = num1 + num2
 print("Sum of this two Number : ",sum)
+
+name = "Rayhan"
+print(type(name))
+name = 2
+print(type(name))
+name = 2.3
+print(type(name))
+name = 2.342322428762426
+print(type(name))
--- math Function.py
+++ math Function.py
@@ -1,4 +1,8 @@
 import math
+print(math.factorial(3))
+print(math.ceil(3.4))
+print(math.floor(3.4))
+print(math.gcd(12,12))
 print("For Area \n")
 Base = float(input("Enter Base : "))
 height = float(input("Enter Height : "))
