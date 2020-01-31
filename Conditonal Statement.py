4e09e29a11933fb6dabfcd775a87b49f00f790fc
NMRayhan <nurmdrayhan97@gmail.com>
2020-01-31 17:33:47 +0600

    Conditonal Statement

---
+++ Conditonal Statement.py
@@ -0,0 +1,15 @@
+mark1= int(input("Enter Mark : "))
+if(mark1>=80):
+    print("You Have A+ Grade \n")
+elif(mark1>=70):
+    print("You Have A Grade \n")
+elif(mark1>=65):
+    print("You Have A- Grade \n")
+elif(mark1>=60):
+    print("You Have B Grade \n")
+elif(mark1>=50):
+    print("You Have C Grade \n")
+elif(mark1>=40):
+    print("You Have D Grade \n")
+else:
+    print("Fail\n")
