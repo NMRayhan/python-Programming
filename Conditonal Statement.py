ba89d2dec28ac85f657af2b25fd6c760091bd778
NMRayhan <nurmdrayhan97@gmail.com>
2020-01-31 20:10:11 +0600

    Checking Highest Mark in Using if elif

--- Conditonal Statement.py
+++ Conditonal Statement.py
@@ -1,4 +1,6 @@
-mark1= int(input("Enter Mark : "))
+
+mark1 = int(input("Enter Mark1 : "))
+mark2 = int(input("Enter Mark2 : "))
 '''if(mark1>=80):
     print("You Have A+ Grade \n")
 elif(mark1>=70):
@@ -13,11 +15,19 @@ elif(mark1>=40):
     print("You Have D Grade \n")
 else:
     print("Fail\n")'''
-if(mark1==40):
+'''if(mark1==40):
     print("Pass ")
 if(mark1>=40):
     print("Pass")
 if(mark1<40):
     print("Fail")
 if(mark1>=50):
-    print("Pass >= 50")
+    print("Pass >= 50")'''
+if(mark1>mark2):
+    print("Mark1 is Highest")
+elif(mark2>mark1):
+    print("Mark2 is Highest")
+elif(mark1==mark2):
+    print("Mark 1 and Mark2 is Same Value ")
+else:
+    print("Nothing to else ")
