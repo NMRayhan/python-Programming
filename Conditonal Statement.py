a4f9bc049695e9af9068e939866961241349e200
NMRayhan <nurmdrayhan97@gmail.com>
2020-01-31 19:06:58 +0600

    Check Using Only Multiple If Statement

--- Conditonal Statement.py
+++ Conditonal Statement.py
@@ -1,5 +1,5 @@
 mark1= int(input("Enter Mark : "))
-if(mark1>=80):
+'''if(mark1>=80):
     print("You Have A+ Grade \n")
 elif(mark1>=70):
     print("You Have A Grade \n")
@@ -12,4 +12,12 @@ elif(mark1>=50):
 elif(mark1>=40):
     print("You Have D Grade \n")
 else:
-    print("Fail\n")
+    print("Fail\n")'''
+if(mark1==40):
+    print("Pass ")
+if(mark1>=40):
+    print("Pass")
+if(mark1<40):
+    print("Fail")
+if(mark1>=50):
+    print("Pass >= 50")
