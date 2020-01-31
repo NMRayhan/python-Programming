7149b4d92380d31c9bef5eab57d48891ce8a8efb
NMRayhan <nurmdrayhan97@gmail.com>
2020-01-31 17:44:04 +0600

    Print Line With Out New Line using (end=" ")

--- 1stProgram.py
+++ 1stProgram.py
@@ -1,13 +1,21 @@
+print("I'm Nur Mohammad Rayhan", end=", ")
+print("I'm Studying Software Engineering", end=", ")
+print("I'm always Speed my Time With My Laptop Because I Love My Laptop")
 number1 = 10
 number2 = "Hello"
+
 n = type(number2)
 print(n)
+
 n = type(number1)
 print(n)
+
 n = type(002.972)
 print(n)
+
 n = type(bool)
 print(n)
+
 a = 1
 b = 3.4
 print(a+b)
