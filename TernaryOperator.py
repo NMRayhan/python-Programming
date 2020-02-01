f7b7fed167848adbe39f05169b4174816b7b46f7
NMRayhan <nurmdrayhan97@gmail.com>
2020-02-01 19:23:37 +0600

    Using TernaryOperator in Conditonal Statement

--- TernaryOperator.py
+++ TernaryOperator.py
@@ -1,5 +1,33 @@
-number1 = int(input("Enter Any Possitive Number "))
+'''number1 = int(input("Enter Any Possitive Number "))
 number2 = int(input("Enter Another Possitive Number "))
-
+number3 = int(input("Enter Another Possitive Number "))
+'''
+'''
+if(number1>number2):
+    print(number1)
+else:
+    print(number2)
+'''
+'''
 maximum_Number = number1 if number1>number2 else number2
 print(f"{maximum_Number} is Maximum Number ")
+'''
+'''
+if(number1>number2):
+    if(number1>number3):
+        print(number1)
+    else:
+        print(number3)
+maximum_Number = number1 if number1>number2  number1>number3 else number3
+'''
+item_a = 32.3
+item_b = 30.1
+
+if item_a>item_b:
+    Cheapset = item_a
+else:
+    Cheapset = item_b
+print(f"Cheapset price in ${Cheapset}")
+
+
+
--- TypeCasting.py
+++ TypeCasting.py
@@ -2,7 +2,7 @@ num1 = int(input("Enter first Number : "))
 num2 = int(input("Enter Second Number : "))
 
 sum = num1 + num2
-print("Sum of this two Number : ",sum)
+print("Sum of this two Number : ", sum)
 name = "Rayhan"
 print(type(name))
 name = 2
@@ -15,3 +15,6 @@ name = True
 print(type(name))
 #formate
 print(f"{num1} + {num2} = {num1+num2}")
+a = a10 =20
+print(type(a))
+print(a)
