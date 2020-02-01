f9eece490461edb92a8c1cba18c1aa23c69ac221
NMRayhan <nurmdrayhan97@gmail.com>
2020-02-01 21:40:19 +0600

    Boolen Data Type Return False

--- Logial Operator.py
+++ Logial Operator.py
@@ -1,4 +1,6 @@
 #leap Year
+'''
+
 year = int(input("Enter Year Number : "))
 
 if year%400==0:
@@ -8,3 +10,10 @@ elif year%4==0 and year%100!=0:
 else:
     Result = "Not Leap Year"
 print(Result)
+'''
+print(bool(""))#empty String
+print(bool(0))#0 Return False
+print(bool(False))#Return False
+print(bool({}))#Return False
+print(bool([]))#Return False
+print(bool(()))#Return False
