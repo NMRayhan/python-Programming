ddbb45da5fa1c2183843ac323e1e12d06eeef68a
NMRayhan <nurmdrayhan97@gmail.com>
2020-02-01 16:08:30 +0600

    Using TernaryOperator in Conditonal Statement

---
+++ TernaryOperator.py
@@ -0,0 +1,5 @@
+number1 = int(input("Enter Any Possitive Number "))
+number2 = int(input("Enter Another Possitive Number "))
+
+maximum_Number = number1 if number1>number2 else number2
+print(f"{maximum_Number} is Maximum Number ")
