08ded0a458c47ebe8023bf27d4cd825462b2ecb7
NMRayhan <nurmdrayhan97@gmail.com>
2020-02-01 15:17:03 +0600

    Checking Highest Mark Using Inner if elif Conditional Statement

--- Conditonal Statement.py
+++ Conditonal Statement.py
@@ -1,5 +1,5 @@
 
-mark1, mark2, mark3 = int(input("Enter Mark1 : ")), int(input("Enter Mark2 : ")), int(input("Enter Mark3 : "))
+mark1, mark2, mark3, mark4 = int(input("Enter Mark1 : ")), int(input("Enter Mark2 : ")), int(input("Enter Mark3 : ")), int(input("Enter Mark4 : "))
 '''if(mark1>=80):
     print("You Have A+ Grade \n")
 elif(mark1>=70):
@@ -32,16 +32,33 @@ else:
     print("Nothing to else ")'''
 if(mark1>mark2):
     if(mark1>mark3):
-        print("Mark 1 is Highest mark ")
+        if(mark1>mark4):
+            print(f"{mark1} is Highest mark ")
+        else:
+            print(f"{mark4} is Highest Mark ")
     else:
-        print("Mark 3 is Highest Mark ")
+        print(f"{mark3} is Highest Mark")
 elif(mark2>mark3):
-    if(mark2>mark1):
-        print("Mark 2 is Highest Mark ")
+    if(mark2>mark4):
+        if(mark2>mark1):
+            print(f"{mark2} is Highest Mark ")
+        else:
+            print(f"{mark1} is Highest Mark ")
     else:
-        print("Mark 1 is Highest Mark ")
+        print(f"{mark4} is Highest Mark ")
 elif(mark3>mark1):
     if(mark3>mark2):
-        print("Mark 3 is Highest Mark ")
+        if(mark3>mark4):
+            print(f"{mark3} is Highest Mark ")
+        else:
+            print(f"{mark4} is Highest Mark ")
     else:
-        print("Mark 2 is Highest Mark ")
+        print(f"{mark2} is Highest Mark ")
+elif(mark4>mark1):
+    if(mark4>mark2):
+        if(mark4>mark3):
+            print(f"{mark4} is Highest Mark ")
+        else:
+            print(f"{mark3} is Highest Mark ")
+    else:
+        print(f"{mark2} is Highest Mark ")
