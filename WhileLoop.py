1c035a319301aa69258f829de6b1847be59eb78f
NMRayhan <nurmdrayhan97@gmail.com>
2020-02-19 23:30:44 +0600

    Average Decrement Using While Loop

--- WhileLoop.py
+++ WhileLoop.py
@@ -1,8 +1,9 @@
 sum = 0.0 #initiialize
-initial = 0
-while initial < 10:
+initial = 10
+divider = initial
+while initial > 0:
     number = int(input(f"Enter Number {initial} = "))
     sum = sum + number
-    initial=initial+1 #increment and end of the while loop
-print(f"Total Average is {sum/initial} and Total is {sum}")
+    initial=initial-1 #increment and end of the while loop
+print(f"Total Average is {sum/divider} and Total is {sum}")
 print("End of Program")
