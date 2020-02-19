515a377c071e3453a48b0de9998587a66aa1a224
NMRayhan <nurmdrayhan97@gmail.com>
2020-02-19 23:07:39 +0600

    Comment

--- WhileLoop.py
+++ WhileLoop.py
@@ -1,7 +1,8 @@
+#initiialize
 sum = 0
 initial = 1
 while initial <= 10:
     sum = sum + initial
-    initial=initial+1
+    initial=initial+1 #increment and end of the while loop
 print("Sum of This Range ",sum)
 print("End of Program")
