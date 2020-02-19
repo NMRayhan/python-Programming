9acd629464033ebe14d95025e8222555397742b8
NMRayhan <nurmdrayhan97@gmail.com>
2020-02-19 23:22:38 +0600

    Average using While Loop

--- WhileLoop.py
+++ WhileLoop.py
@@ -1,8 +1,8 @@
-#initiialize
-sum = 0
-initial = 1
-while initial <= 10:
-    sum = sum + initial
+sum = 0.0 #initiialize
+initial = 0
+while initial < 10:
+    number = int(input(f"Enter Number {initial} = "))
+    sum = sum + number
     initial=initial+1 #increment and end of the while loop
-print("Sum of This Range ",sum)
+print(f"Total Average is {sum/initial} and Total is {sum}")
 print("End of Program")
